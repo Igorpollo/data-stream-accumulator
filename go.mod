ee0792adcf8080882a07b1267119cdfedcf1c592
firmino <firminodefaria@gmail.com>
2019-07-30 13:19:11 -0300

    feat: new data structure

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.12
 require (
 	github.com/Igorpollo/go-custom-log v0.0.0-20190726142012-24d78c3e12b8 // indirect
 	github.com/buaazp/fasthttprouter v0.1.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fasthttp/router v0.4.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,49 +1,59 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"log"
-	"encoding/json"
-	"os"
+	"net"
+	"time"
+	"unsafe"
+
+	"github.com/davecgh/go-spew/spew"
 	"github.com/fasthttp/router"
 	"github.com/valyala/fasthttp"
 )
 
-type dataRecieve struct {
-	body interface{}
+const (
+	DTYPE_JSON = iota
+	DTYPE_STRING
+	DTYPE_ARRAY_STRING
+	DTYPE_IMAGE
+	DTYPE_AUDIO
+	DTYPE_OTHER
+)
+
+type dataPackage struct {
+	uuid             string
+	dateTimeReceived time.Time
+	dataSize         uintptr
+	dataType         int
+	data             string
+	ownerID          int
+	ip               net.IP
 }
 
+// asdfasdf
 func Index(ctx *fasthttp.RequestCtx) {
-	var result map[string]interface{}
 
-	// Unmarshal or Decode the JSON to the interface.
-	json.Unmarshal([]byte(ctx.PostBody()), &result)
-	
-	empData, err := json.Marshal(result)   
-    if err != nil {
-        fmt.Println(err.Error())
-        return
+	dataBody := ctx.PostBody()
+
+	dataStructure := dataPackage{
+		dateTimeReceived: time.Now(),
+		dataSize:         unsafe.Sizeof(dataBody),
+		dataType:         DTYPE_JSON,
+		data:             base64.StdEncoding.EncodeToString(dataBody),
+		ownerID:          1,
+		ip:               ctx.RemoteIP(),
+		uuid:             "asdfçlkasjdf",
 	}
 
-	jsonStr := string(empData)
-    fmt.Println("The JSON data is:")
-    fmt.Println(jsonStr)
+	fmt.Println("The JSON data is:")
+	spew.Dump(dataStructure)
 
-	
-	f, err := os.Create("test.json")
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    _, err = f.WriteString(jsonStr)
-    if err != nil {
-        fmt.Println(err)
-        f.Close()
-        return
-    }
 	ctx.WriteString("Welcome!")
 }
 
+// asdf
 func Hello(ctx *fasthttp.RequestCtx) {
 	fmt.Fprintf(ctx, "Hello, %s!\n", ctx.UserValue("name"))
 }
